8c9f72c871267350a8040a64d574d72263a7b521
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-03-23 10:36:25 -0700

    intern test created and passed

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,1 +1,21 @@
 const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+    test("Can set school with constuctor", () => {
+      const school = "college";
+      const e = new Intern("Nick", 77, "something@email.com", school);
+  
+      expect(e.school).toBe(school);
+    });
+    test("Can get role via getRole() method", () => {
+      const e = new Intern("Nick", 77, "something@email.com", "school");
+  
+      expect(e.getRole()).toBe("Intern");
+    });
+    test("Can get school via getSchool() method", () => {
+      const school = "college";
+      const e = new Intern("Nick", 77, "something@email.com", school);
+  
+      expect(e.getSchool()).toBe(school);
+    });
+  });
--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,1 +1,21 @@
 const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    test("Can set office number with constuctor", () => {
+      const officeNum = "520.123.4567";
+      const e = new Manager("Nick", 77, "something@email.com", officeNum);
+  
+      expect(e.officeNum).toBe(officeNum);
+    });
+    test("Can get role via getRole() method", () => {
+      const e = new Manager("Nick", 77, "something@email.com", "officeNum");
+  
+      expect(e.getRole()).toBe("Manager");
+    });
+    test("Can get office number via getOfficeNum() method", () => {
+      const officeNum = "520.123.4567";
+      const e = new Manager("Nick", 77, "something@email.com", officeNum);
+  
+      expect(e.getOfficeNum()).toBe(officeNum);
+    });
+  });
